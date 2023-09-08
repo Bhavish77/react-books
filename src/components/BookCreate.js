e11bd1907a099793365c8edf9a956aa234ae845d
Bhavish <bhavishmayyar77@gmail.com>
2023-09-08 15:06:41 +0530

    first commit

---
+++ src/components/BookCreate.js
@@ -0,0 +1,29 @@
+import { useState, useContext } from "react";
+import BooksContext from "../context/book";
+
+function BookCreate() {
+  const [title, setTitle] = useState("");
+  const { createBook } = useContext(BooksContext);
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createBook(title);
+    setTitle("");
+  };
+
+  return (
+    <div className="book-create">
+      <form onSubmit={handleSubmit}>
+        <label>title</label>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create!</button>
+      </form>
+    </div>
+  );
+}
+
+export default BookCreate;
